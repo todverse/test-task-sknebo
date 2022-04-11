d0c9048c6ec09c9bf59ae0414e14e69d20bcbdbf
todverse <todverse0@gmail.com>
2022-04-11 06:11:50 +0400

    add closest route from click-mark to MCAR

--- src/components/MapYa.vue
+++ src/components/MapYa.vue
@@ -10,8 +10,8 @@
   >
   <ymap-marker 
       :coords="coords" 
-      marker-id="123" 
-      :hint-content="hint" 
+      marker-id="1" 
+      :hint-content="address" 
   />
   </yandex-map>
 </div>
@@ -20,37 +20,55 @@
 <script>
   import { yandexMap, ymapMarker } from 'vue-yandex-maps'
   import Notifications from './Notifications'
+
   export default {
     components : { yandexMap, ymapMarker, Notifications },
+
       data: () => ({
        coords: [
         55.753429006938546,
         37.63096318866614
       ],
-      hint: 'some hint',
+
       settings: {
         apiKey: '99a2f189-c861-4b8a-90e7-34fa2c7add0e',
       },
-      address: String,
+
+      address: 'Россия, Москва',
       instance: Object,
       Router: Object,
       counter: 0,
+      MCARmarks: [],
     }),
+
     methods: {
       onClick(e) {
       this.coords = e.get('coords');
+
+      let closestMCAR = ymaps.geoQuery(this.MCARmarks).getClosestTo(this.coords);
+      
+      if(this.counter === 0) { 
       this.Router = new ymaps.multiRouter.MultiRoute({   
         referencePoints: [
           e.get('coords'),
-          'МКАД ',
+          closestMCAR,
         ]
         }, {
-        boundsAutoApply: true
+        boundsAutoApply: false
       });
-        this.Router.model.setReferencePoints([
-          this.coords,
-          'МКАД ',
-        ]);
+      this.counter++;
+      };
+
+      this.Router.model.setReferencePoints([
+        this.coords,
+        closestMCAR,
+      ]);
+
+      this.Router.model.events.add('requestsuccess', () => {
+        let activeRouter = this.Router.getActiveRoute();
+        console.log(activeRouter.getPaths().properties.get('distance'));
+      });
+
       ymaps.geocode(e.get('coords')).then((res) => {
       this.instance.geoObjects.add(this.Router);
       this.address = res.geoObjects.get(0).properties.get('metaDataProperty').GeocoderMetaData.text;
@@ -58,10 +76,29 @@
       setTimeout(() => {
         document.getElementById('notification').style.display = 'none';
       }, 1500);
-      })
+      });
+
      },
+     
     init(e) {
       this.instance = e;
+      let marks = [
+        [55.785017, 37.841576],
+        [55.861979, 37.765992],
+        [55.898533, 37.635961],
+        [55.888897, 37.48861],
+        [55.83251, 37.395275],
+        [55.744789, 37.370248],
+        [55.660424, 37.434424],
+        [55.5922, 37.526366],
+        [55.574019, 37.683167],
+        [55.62913, 37.802473],
+        [55.712203, 37.837121]
+      ];
+
+      marks.map((elem, index) => {
+        this.MCARmarks[index] = new ymaps.Placemark(elem);
+      });
     }
     },
   }
